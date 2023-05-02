812c1f1302222331ba5e256f01c472dea857d169
QuietImCoding <dmonte3210@gmail.com>
2023-05-02 12:51:53 -0700

    fixed not using --json in memebot. added jpeg extent to restrict file size to 1mb

--- memebot/memebot.sh
+++ memebot/memebot.sh
@@ -3,13 +3,13 @@ send_meme() {
     echo $post
     template="$(echo $post | cut -d' ' -f2 | sed 's/[^a-z]//g')"
     echo $template
-    tmpfname="${template}_$RANDOM.png"
+    tmpfname="${template}_$RANDOM.jpg"
     text="$(echo $post | cut -d' ' -f3-)"
     fontsize=$((2000/(((${#text}+1)/7)**2+1)))
     fontsize=$((fontsize>20 ? $fontsize : 20))
     fontsize=$((fontsize<50 ? $fontsize : 50))
     #convert "./templates/${template}.png" -gravity South -size 800x -pointsize $fontsize caption:"$text" +swap -append +0+800  $PWD/$tmpfname
-    convert "./templates/${template}.png" \( \( -gravity north -splice 0x50 \) -size 800x -pointsize 30 caption:"$text" \( -gravity south -splice 0x50 \)  \) -append $PWD/$tmpfname
+    convert "./templates/${template}.png" \( \( -gravity north -splice 0x50 \) -size 800x -pointsize 30 caption:"$text" \( -gravity south -splice 0x50 \)  \) -append -define jpeg:extent=1000KB $PWD/$tmpfname
     echo "Created image with text $text in $PWD/$tmpfname"
     bsky post -r $1 -i $PWD/$tmpfname "$template meme with text: $text" && rm $tmpfname
 }
@@ -24,5 +24,5 @@ send_reply() {
 }
 
 while true; do 
-    bsky stream --pattern '(?i)/meme' 2>/dev/null | jq -r --unbuffered '"at://"+.did+"/"+.path' | send_reply
+    bsky stream --json --pattern '(?i)/meme' 2>/dev/null | jq -r --unbuffered '"at://"+.did+"/"+.path' | send_reply
 done
