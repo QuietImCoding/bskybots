cc926b45b00e386296f631137c30bcceba68261c
QuietImCoding <dmonte3210@gmail.com>
2023-05-03 14:00:36 -0700

    added new-animal script and created cat/mouse services

---
+++ borkr.sh
@@ -0,0 +1,32 @@
+source .env
+
+get_puppy() {
+    curl -s https://api.flickr.com/services/rest\?api_key\=$FLICKR_KEY\&method\=flickr.photos.search\&text\=puppy%20puppies%20dog\&sort\=interestingness-desc\&content_type\=0\&format\=json | sed 's/jsonFlickrApi//g' | sed -E -e 's/.{14}$//' -e 's/^.{11}//' | jq '.photo[] | "https://api.flickr.com/services/rest?api_key=" + env.FLICKR_KEY + "&method=flickr.photos.getSizes&format=json&photo_id="+.id' | shuf | head -n 1 | xargs curl -s | sed -E -e 's/^.{14}//' | jq -cr '.sizes.size[]|.source' | tail -n 4 | head -n 1 | xargs wget -O puppy.jpg 2>/dev/null
+    imsize=$(du -k puppy.jpg | awk '{ print $1 }')
+    if [[ $imsize -gt 1000 ]]; then
+	echo "puppy was to big... shrinking"
+	mogrify -define jpeg:extent=1000KB puppy.jpg 
+	imsize=$(du -k puppy.jpg | awk '{ print $1 }')
+	echo "puppy is now $imsize kb"
+    fi
+}
+
+bork() { 
+    get_puppy
+    bsky post -r "$1" -i "$PWD/puppy.jpg" HONK
+    echo "HONKED AT $1"
+}
+
+send_reply() {
+    while read -r target;
+    do
+	bork $target &
+    done
+}
+
+while true;
+do
+    bsky stream --json --pattern '(?i)/bork' 2>/dev/null | jq --unbuffered -r '"at://"+.did+"/"+.path' | send_reply
+done
+
+	
---
+++ new_animal.sh
@@ -0,0 +1,12 @@
+read -p "sound: " sound 
+read -p "cute name: " cutename
+read -p "plural name: " pluralname
+read -p "category: " category
+
+cat honkbot/honkr.sh | sed -e "s/honk/$sound/g" \
+			   -e "s/goose/$cutename/g" \
+			   -e "s/geese/$pluralname/g" \
+			   -e "s/bird/$category/g" | tee ${sound}r.sh
+
+cat services/honk.service | sed "s/honkr/${sound}r/g" | tee services/$sound.service
+
