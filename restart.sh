cf5549f79271a22b2bb7ccacf72484786ed9823d
business goose <business@goose.art>
2023-05-06 03:54:15 +0000

    updated restart script to not kill ALL bsky instances, fixed ribbit service to point to ribbitr

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-sudo systemctl stop $1
-ps aux | grep $1 | awk '{ print $2 }' | sudo xargs kill
-sudo killall bsky
+sudo systemctl daemon-reload
+sudo systemctl stop $1 &&
+    ps aux | grep $1 | awk '{ print $2 }' | sudo xargs kill;
 sudo systemctl start $1
